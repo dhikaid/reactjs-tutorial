5636fa0fd749d094c5eec7494dd635a0a2c7bd22
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-11-22 16:13:32 +0700

    vid3: components dan props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,30 +1,24 @@
+const Button = (props) => {
+  // warna default
+  const { children = "Ini Button", variant = "bg-black" } = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-        type="submit"
-      >
-        Buy now
-      </button>
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-        type="submit"
-      >
-        Buy now
-      </button>
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-        type="submit"
-      >
-        Buy now
-      </button>
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-        type="submit"
-      >
-        Buy now
-      </button>
+      <div className="flex gap-x-4">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-black">Register</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
