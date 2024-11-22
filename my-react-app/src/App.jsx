7c5e977a3f500b63e466fdf3e3768b81791b8f04
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-11-22 15:52:31 +0700

    Vid: 1-2 Welcome React!

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    </div>
+  );
+}
+
+export default App;
