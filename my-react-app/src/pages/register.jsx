d02b2a06c99b86e0fd703f9ee2ab7cbc667068b7
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-11-23 00:45:50 +0700

    vid6: reaectrouter

--- my-react-app/src/pages/register.jsx
+++ my-react-app/src/pages/register.jsx
@@ -1,10 +1,17 @@
 import AuthLayout from "../components/Layouts/AuthLayouts";
 import FormRegister from "../components/Fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Login">
+    <AuthLayout title="Register">
       <FormRegister></FormRegister>
+      <p className="text-sm text-center mt-5">
+        Have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>
     </AuthLayout>
   );
 };
